e07ddba6f310e95dc2a06788aac79562565503bd
HmdnnVPn <birumuda2423@gmail.com>
2024-11-12 14:24:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function downloadConfig(provider) {
     
     switch (provider) {
         case 'axis':
-            url = 'youtube.com'; // Sesuaikan path file
+            url = 'https://sfile.mobi/download/73aTzpgte8C?fid=NzNhVHpwZ3RlOEMma2V5PUYxRDI1OUY0MzlBN0ZFMzc-'; // Sesuaikan path file
             break;
         case 'telkomsel':
             url = 'configs/telkomsel.ehi'; // Sesuaikan path file
